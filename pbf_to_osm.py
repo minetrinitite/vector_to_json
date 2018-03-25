b4e804c75d1201613facae69c77085fb30267ba5
minetrinitite <dartweder7@gmail.com>
2018-03-25 04:14:22 +0300

    pbf to osm function

--- pbf_to_osm.py
+++ pbf_to_osm.py
@@ -1,8 +1,9 @@
-import subprocess, os
-import db
+import subprocess
 
 def pbf_to_osm(file, osmosis):
 
-commandstring = osmosis + " --read-pbf " + readName + " --write-xml " + writeName + ".osm"
+    commandstring = osmosis + " --read-pbf " + file + " --write-xml " + file[:-8] + ".osm"
 
-proc = subprocess.Popen(commandstring, stdout=subprocess.PIPE)
+    proc = subprocess.Popen(commandstring, stdout=subprocess.PIPE)
+    proc.wait()
+    return file[:-4]
